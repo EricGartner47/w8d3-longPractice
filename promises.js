3b347113fd6859ce09bf93bee9b762b2e08535ed
Your Name <email@example.com>
2021-10-13 15:50:01 -0400

    part 1 - 4

--- promises.js
+++ promises.js
@@ -1,28 +1,55 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here
+// const num1 = () => {
+//     return 47;
+// }
 
+// async function num2() {
+//     return 48;
+// }
 
+// console.log('num1', num1());
+// console.log('num2', num2());
+
+// num2().then(result => console.log(result));
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here
+// async function num3() { //why did it not print "Promise"
+// - when using await, return value is the fulfilled value of promise
+//     const value = await num2();
+//     console.log('num3', value)
+// }
 
+// num3()
 
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here
+// async function awaitingPromise() {
+//     const p = new Promise((resolve)=> {
+//         setTimeout(()=> {
+//             resolve("I'm a promise")
+//         }, 2000)
+//     });
+//     const res = await p;
+//     console.log('my promise is', res)
+// }
 
+// awaitingPromise()
 
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
 
-// Your code here
+// new Promise((resolve)=> {
+//     setTimeout(()=> {
+//         resolve('done!')
+//     }, 1500)
+// }).then(r => console.log('then my other promise is', r))
 
 
 
